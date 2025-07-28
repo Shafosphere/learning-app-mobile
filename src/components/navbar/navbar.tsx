ed4883117e73deceed7fda6929cdd25e0712c9ba
Shafosphere <117680528+Shafosphere@users.noreply.github.com>
2025-07-28 15:28:31 +0200

    Navbar update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,21 +1,66 @@
 import { useRouter } from "expo-router";
-import { styles } from "./styles_navbar";
-import { Text, TouchableOpacity, View } from "react-native";
+import {
+  Platform,
+  Pressable,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { useStyles } from "./styles_navbar";
+import { Image } from "expo-image";
+
+import Ionicons from "@expo/vector-icons/Ionicons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Entypo from "@expo/vector-icons/Entypo";
+
+const logo = require("./../../../assets/box/logo.png");
 
 export default function Navbar() {
   const router = useRouter();
+  const styles = useStyles();
+  const topPad = Platform.OS === "android" ? StatusBar.currentHeight : 0;
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { marginTop: topPad }]}>
       <TouchableOpacity onPress={() => router.push("/")}>
-        <Text style={styles.link}>Home</Text>
+        <Image source={logo} style={styles.logo} />
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push("/settings")}>
-        <Text style={styles.link}> Settings</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => router.push("/flashcards")}>
-        <Text style={styles.link}> Flashcards</Text>
-      </TouchableOpacity>
+
+      <Pressable
+        style={({ pressed }) => [
+          styles.iconCon,
+          pressed && styles.iconConPressed,
+        ]}
+        onPress={() => router.push("/settings")}
+      >
+        <Entypo style={styles.icon} name="home" size={16} color="black" />
+      </Pressable>
+
+      <Pressable
+        style={({ pressed }) => [
+          styles.iconCon,
+          pressed && styles.iconConPressed,
+        ]}
+        onPress={() => router.push("/flashcards")}
+      >
+        <FontAwesome5 name="box-open" size={16} style={styles.icon} />
+      </Pressable>
+
+      <Pressable
+        style={({ pressed }) => [
+          styles.iconCon,
+          pressed && styles.iconConPressed,
+        ]}
+        onPress={() => router.push("/settings")}
+      >
+        <Ionicons
+          style={styles.icon}
+          name="settings-sharp"
+          size={16}
+          color="black"
+        />
+      </Pressable>
     </View>
   );
 }
--- src/components/navbar/styles_navbar.ts
+++ src/components/navbar/styles_navbar.ts
@@ -1,16 +1,31 @@
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        height: 60,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'black',
-    },
-    link: {
-        fontSize: 24,
-        color: 'white',
-        fontWeight: 'bold',
-    }
-})
+import { createThemeStylesHook } from "@/src/theme/createThemeStylesHook";
+const LOGO_ASPECT_RATIO = 523 / 555;
+export const useStyles = createThemeStylesHook((colors) => ({
+  container: {
+    flexDirection: "row",
+    height: 55,
+    alignItems: "center",
+    justifyContent: "flex-start",
+    backgroundColor: colors.secondBackground,
+    padding: 7,
+    paddingLeft: 14,
+  },
+  logo: {
+    width: 40,
+    aspectRatio: LOGO_ASPECT_RATIO,
+    marginRight: 20,
+  },
+  icon: {
+    color: colors.headline,
+  },
+  iconCon: {
+    paddingTop: 5,
+    paddingBottom: 5,
+    padding: 3,
+    borderRadius: 5,
+    marginLeft: 1,
+  },
+  iconConPressed: {
+    backgroundColor: colors.my_green,
+  },
+}));
